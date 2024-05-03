98c20e5bd3279f624d047593a35b530edf204862
mercury-1337 <103053455+mercury-1337@users.noreply.github.com>
2024-05-03 14:28:31 +0300

    Add files via upload

---
+++ Fish_Bot.lua/Fish_Bot-bytecode.asm
@@ -0,0 +1,292 @@
+-- Source: C:\Users\mrxcry\Desktop\Work\protect\crack-tools\Fish_Bot.lua
+-- Compiler version: 2.1
+-- Flags: 0x2
+
+-- Proto #1 -- pos: 0x5, size: 0x280, fullsize: 0x282, flags: 0x0, params: 0, framesize: 0x3, numuv: 0, numkgc: 65, numkn: 0, numbc: 81, ins: 0xE
+0x0000000E | 0000    35:(TDUP  )   0   4
+0x00000012 | 0004    35:(TDUP  )   1   1
+0x00000016 | 0008    35:(TDUP  )   2   0
+0x0000001A | 000C    3D:(TSETS )   2   1   2  ; "*m*"
+0x0000001E | 0010    35:(TDUP  )   2   3
+0x00000022 | 0014    3E:(TSETB )   2   1   1
+0x00000026 | 0018    3D:(TSETS )   1   0   5  ; "^!^"
+0x0000002A | 001C    35:(TDUP  )   1   7
+0x0000002E | 0020    35:(TDUP  )   2   6
+0x00000032 | 0024    3D:(TSETS )   2   1   8  ; "*n*"
+0x00000036 | 0028    35:(TDUP  )   2   9
+0x0000003A | 002C    3E:(TSETB )   2   1   1
+0x0000003E | 0030    3D:(TSETS )   1   0  10  ; "^@^"
+0x00000042 | 0034    35:(TDUP  )   1  12
+0x00000046 | 0038    35:(TDUP  )   2  11
+0x0000004A | 003C    3D:(TSETS )   2   1  13  ; "*b*"
+0x0000004E | 0040    35:(TDUP  )   2  14
+0x00000052 | 0044    3E:(TSETB )   2   1   1
+0x00000056 | 0048    3D:(TSETS )   1   0  15  ; "^^"
+0x0000005A | 004C    35:(TDUP  )   1  17
+0x0000005E | 0050    35:(TDUP  )   2  16
+0x00000062 | 0054    3D:(TSETS )   2   1  18  ; "*v*"
+0x00000066 | 0058    35:(TDUP  )   2  19
+0x0000006A | 005C    3E:(TSETB )   2   1   1
+0x0000006E | 0060    3D:(TSETS )   1   0  20  ; "^$^"
+0x00000072 | 0064    35:(TDUP  )   1  22
+0x00000076 | 0068    35:(TDUP  )   2  21
+0x0000007A | 006C    3D:(TSETS )   2   1  23  ; "*c*"
+0x0000007E | 0070    35:(TDUP  )   2  24
+0x00000082 | 0074    3E:(TSETB )   2   1   1
+0x00000086 | 0078    3D:(TSETS )   1   0  25  ; "^%^"
+0x0000008A | 007C    35:(TDUP  )   1  27
+0x0000008E | 0080    35:(TDUP  )   2  26
+0x00000092 | 0084    3D:(TSETS )   2   1  28  ; "*x*"
+0x00000096 | 0088    35:(TDUP  )   2  29
+0x0000009A | 008C    3E:(TSETB )   2   1   1
+0x0000009E | 0090    3D:(TSETS )   1   0   5  ; "^!^"
+0x000000A2 | 0094    35:(TDUP  )   1  31
+0x000000A6 | 0098    35:(TDUP  )   2  30
+0x000000AA | 009C    3D:(TSETS )   2   1  32  ; "*z*"
+0x000000AE | 00A0    35:(TDUP  )   2  33
+0x000000B2 | 00A4    3E:(TSETB )   2   1   1
+0x000000B6 | 00A8    3D:(TSETS )   1   0  34  ; "^&^"
+0x000000BA | 00AC    35:(TDUP  )   1  36
+0x000000BE | 00B0    35:(TDUP  )   2  35
+0x000000C2 | 00B4    3D:(TSETS )   2   1  32  ; "*z*"
+0x000000C6 | 00B8    35:(TDUP  )   2  37
+0x000000CA | 00BC    3E:(TSETB )   2   1   1
+0x000000CE | 00C0    3D:(TSETS )   1   0  38  ; "^*^"
+0x000000D2 | 00C4    35:(TDUP  )   1  40
+0x000000D6 | 00C8    35:(TDUP  )   2  39
+0x000000DA | 00CC    3D:(TSETS )   2   1  41  ; "*a*"
+0x000000DE | 00D0    35:(TDUP  )   2  42
+0x000000E2 | 00D4    3E:(TSETB )   2   1   1
+0x000000E6 | 00D8    3D:(TSETS )   1   0  43  ; "^(^"
+0x000000EA | 00DC    35:(TDUP  )   1  45
+0x000000EE | 00E0    35:(TDUP  )   2  44
+0x000000F2 | 00E4    3D:(TSETS )   2   1  46  ; "*s*"
+0x000000F6 | 00E8    35:(TDUP  )   2  47
+0x000000FA | 00EC    3E:(TSETB )   2   1   1
+0x000000FE | 00F0    3D:(TSETS )   1   0  48  ; "^)^"
+0x00000102 | 00F4    35:(TDUP  )   1  50
+0x00000106 | 00F8    35:(TDUP  )   2  49
+0x0000010A | 00FC    3D:(TSETS )   2   1  51  ; "*d*"
+0x0000010E | 0100    35:(TDUP  )   2  52
+0x00000112 | 0104    3E:(TSETB )   2   1   1
+0x00000116 | 0108    3D:(TSETS )   1   0  53  ; "^q^"
+0x0000011A | 010C    35:(TDUP  )   1  55
+0x0000011E | 0110    35:(TDUP  )   2  54
+0x00000122 | 0114    3D:(TSETS )   2   1  56  ; "*f*"
+0x00000126 | 0118    35:(TDUP  )   2  57
+0x0000012A | 011C    3E:(TSETB )   2   1   1
+0x0000012E | 0120    3D:(TSETS )   1   0  58  ; "^w^"
+0x00000132 | 0124    35:(TDUP  )   1  60
+0x00000136 | 0128    35:(TDUP  )   2  59
+0x0000013A | 012C    3D:(TSETS )   2   1  61  ; "*g*"
+0x0000013E | 0130    35:(TDUP  )   2  62
+0x00000142 | 0134    3E:(TSETB )   2   1   1
+0x00000146 | 0138    3D:(TSETS )   1   0  63  ; "^e^"
+0x0000014A | 013C    37:(GSET  )   0  64      ; "SS"
+0x0000014E | 0140    4B:(RET0  )   0   1
+
+-- Proto #2 -- pos: 0x287, size: 0x8C, fullsize: 0x8E, flags: 0x0, params: 1, framesize: 0xB, numuv: 4, numkgc: 1, numkn: 0, numbc: 31, ins: 0x290
+0x00000290 | 0000    27:(KSTR  )   1   0      ; ""
+0x00000294 | 0004    29:(KSHORT)   2   1
+0x00000298 | 0008    15:(LEN   )   3   0
+0x0000029C | 000C    29:(KSHORT)   4   1
+0x000002A0 | 0010    4D:(FORI  )   2 => 0078
+0x000002A4 | 0014 => 2D:(UGET  )   6   0      ; 
+0x000002A8 | 0018    2D:(UGET  )   7   1      ; 
+0x000002AC | 001C    38:(TGETV )   8   0   5
+0x000002B0 | 0020    42:(CALL  )   7   0   2
+0x000002B4 | 0024    41:(CALLM )   6   2   0
+0x000002B8 | 0028    2D:(UGET  )   7   0      ; 
+0x000002BC | 002C    2D:(UGET  )   8   1      ; 
+0x000002C0 | 0030    2D:(UGET  )   9   2      ; 
+0x000002C4 | 0034    38:(TGETV )  10   0   5
+0x000002C8 | 0038    42:(CALL  )   9   0   2
+0x000002CC | 003C    41:(CALLM )   8   0   0
+0x000002D0 | 0040    41:(CALLM )   7   2   0
+0x000002D4 | 0044    05:(ISNEV )   6   7
+0x000002D8 | 0048    58:(JMP   )   6 => 005C
+0x000002DC | 004C    2D:(UGET  )   6   2      ; 
+0x000002E0 | 0050    38:(TGETV )   7   0   5
+0x000002E4 | 0054    42:(CALL  )   6   2   2
+0x000002E8 | 0058    3C:(TSETV )   6   0   5
+0x000002EC | 005C => 12:(MOV   )   6   1
+0x000002F0 | 0060    27:(KSTR  )   7   0      ; ""
+0x000002F4 | 0064    2D:(UGET  )   8   3      ; 
+0x000002F8 | 0068    38:(TGETV )   9   0   5
+0x000002FC | 006C    42:(CALL  )   8   2   2
+0x00000300 | 0070    26:(CAT   )   1   6   8
+0x00000304 | 0074    4F:(FORL  )   2 => 0014
+0x00000308 | 0078 => 4C:(RET1  )   1   2
+
+-- Proto #3 -- pos: 0x315, size: 0x2EA5E, fullsize: 0x2EA61, flags: 0x0, params: 0, framesize: 0x4, numuv: 0, numkgc: 5, numkn: 0, numbc: 19, ins: 0x31F
+0x0000031F | 0000    36:(GGET  )   0   0      ; "_G"
+0x00000323 | 0004    36:(GGET  )   1   1      ; "G"
+0x00000327 | 0008    3A:(TGETB )   1   1   1
+0x0000032B | 000C    35:(TDUP  )   2   2
+0x0000032F | 0010    42:(CALL  )   1   2   2
+0x00000333 | 0014    38:(TGETV )   0   0   1
+0x00000337 | 0018    36:(GGET  )   1   0      ; "_G"
+0x0000033B | 001C    36:(GGET  )   2   1      ; "G"
+0x0000033F | 0020    3A:(TGETB )   2   2   1
+0x00000343 | 0024    35:(TDUP  )   3   3
+0x00000347 | 0028    42:(CALL  )   2   2   2
+0x0000034B | 002C    38:(TGETV )   1   1   2
+0x0000034F | 0030    36:(GGET  )   2   1      ; "G"
+0x00000353 | 0034    3A:(TGETB )   2   2   1
+0x00000357 | 0038    35:(TDUP  )   3   4
+0x0000035B | 003C    42:(CALL  )   2   0   2
+0x0000035F | 0040    41:(CALLM )   1   0   0
+0x00000363 | 0044    41:(CALLM )   0   2   0
+0x00000367 | 0048    44:(CALLT )   0   1
+
+-- Proto #4 -- pos: 0x2ED76, size: 0x2BE, fullsize: 0x2C0, flags: 0x0, params: 0, framesize: 0x3, numuv: 0, numkgc: 72, numkn: 0, numbc: 87, ins: 0x2ED7F
+0x0002ED7F | 0000    35:(TDUP  )   0   4
+0x0002ED83 | 0004    35:(TDUP  )   1   1
+0x0002ED87 | 0008    35:(TDUP  )   2   0
+0x0002ED8B | 000C    3D:(TSETS )   2   1   2  ; "*h*"
+0x0002ED8F | 0010    35:(TDUP  )   2   3
+0x0002ED93 | 0014    3E:(TSETB )   2   1   1
+0x0002ED97 | 0018    3D:(TSETS )   1   0   5  ; "^r^"
+0x0002ED9B | 001C    35:(TDUP  )   1   7
+0x0002ED9F | 0020    35:(TDUP  )   2   6
+0x0002EDA3 | 0024    3D:(TSETS )   2   1   8  ; "*j*"
+0x0002EDA7 | 0028    35:(TDUP  )   2   9
+0x0002EDAB | 002C    3E:(TSETB )   2   1   1
+0x0002EDAF | 0030    3D:(TSETS )   1   0  10  ; "^t^"
+0x0002EDB3 | 0034    35:(TDUP  )   1  12
+0x0002EDB7 | 0038    35:(TDUP  )   2  11
+0x0002EDBB | 003C    3D:(TSETS )   2   1  13  ; "*k*"
+0x0002EDBF | 0040    35:(TDUP  )   2  14
+0x0002EDC3 | 0044    3E:(TSETB )   2   1   1
+0x0002EDC7 | 0048    3D:(TSETS )   1   0  15  ; "^y^"
+0x0002EDCB | 004C    35:(TDUP  )   1  17
+0x0002EDCF | 0050    35:(TDUP  )   2  16
+0x0002EDD3 | 0054    3D:(TSETS )   2   1  18  ; "*l*"
+0x0002EDD7 | 0058    35:(TDUP  )   2  19
+0x0002EDDB | 005C    3E:(TSETB )   2   1   1
+0x0002EDDF | 0060    3D:(TSETS )   1   0  20  ; "^u^"
+0x0002EDE3 | 0064    35:(TDUP  )   1  22
+0x0002EDE7 | 0068    35:(TDUP  )   2  21
+0x0002EDEB | 006C    3D:(TSETS )   2   1  23  ; "*;*"
+0x0002EDEF | 0070    35:(TDUP  )   2  24
+0x0002EDF3 | 0074    3E:(TSETB )   2   1   1
+0x0002EDF7 | 0078    3D:(TSETS )   1   0  25  ; "^o^"
+0x0002EDFB | 007C    35:(TDUP  )   1  27
+0x0002EDFF | 0080    35:(TDUP  )   2  26
+0x0002EE03 | 0084    3D:(TSETS )   2   1  28  ; "*q*"
+0x0002EE07 | 0088    35:(TDUP  )   2  29
+0x0002EE0B | 008C    3E:(TSETB )   2   1   1
+0x0002EE0F | 0090    3D:(TSETS )   1   0  30  ; "^p^"
+0x0002EE13 | 0094    35:(TDUP  )   1  32
+0x0002EE17 | 0098    35:(TDUP  )   2  31
+0x0002EE1B | 009C    3D:(TSETS )   2   1  33  ; "*w*"
+0x0002EE1F | 00A0    35:(TDUP  )   2  34
+0x0002EE23 | 00A4    3E:(TSETB )   2   1   1
+0x0002EE27 | 00A8    3D:(TSETS )   1   0  35  ; "^a^"
+0x0002EE2B | 00AC    35:(TDUP  )   1  37
+0x0002EE2F | 00B0    35:(TDUP  )   2  36
+0x0002EE33 | 00B4    3D:(TSETS )   2   1  38  ; "*!*"
+0x0002EE37 | 00B8    35:(TDUP  )   2  39
+0x0002EE3B | 00BC    3E:(TSETB )   2   1   1
+0x0002EE3F | 00C0    3D:(TSETS )   1   0  40  ; "^s^"
+0x0002EE43 | 00C4    35:(TDUP  )   1  42
+0x0002EE47 | 00C8    35:(TDUP  )   2  41
+0x0002EE4B | 00CC    3D:(TSETS )   2   1  43  ; "*@*"
+0x0002EE4F | 00D0    35:(TDUP  )   2  44
+0x0002EE53 | 00D4    3E:(TSETB )   2   1   1
+0x0002EE57 | 00D8    3D:(TSETS )   1   0  45  ; "^d^"
+0x0002EE5B | 00DC    35:(TDUP  )   1  47
+0x0002EE5F | 00E0    35:(TDUP  )   2  46
+0x0002EE63 | 00E4    3D:(TSETS )   2   1  48  ; "*#*"
+0x0002EE67 | 00E8    35:(TDUP  )   2  49
+0x0002EE6B | 00EC    3E:(TSETB )   2   1   1
+0x0002EE6F | 00F0    3D:(TSETS )   1   0  50  ; "^f^"
+0x0002EE73 | 00F4    35:(TDUP  )   1  52
+0x0002EE77 | 00F8    35:(TDUP  )   2  51
+0x0002EE7B | 00FC    3D:(TSETS )   2   1  53  ; "*$*"
+0x0002EE7F | 0100    35:(TDUP  )   2  54
+0x0002EE83 | 0104    3E:(TSETB )   2   1   1
+0x0002EE87 | 0108    3D:(TSETS )   1   0  55  ; "^g^"
+0x0002EE8B | 010C    35:(TDUP  )   1  57
+0x0002EE8F | 0110    35:(TDUP  )   2  56
+0x0002EE93 | 0114    3D:(TSETS )   2   1  58  ; "*%*"
+0x0002EE97 | 0118    35:(TDUP  )   2  59
+0x0002EE9B | 011C    3E:(TSETB )   2   1   1
+0x0002EE9F | 0120    3D:(TSETS )   1   0  60  ; "^h^"
+0x0002EEA3 | 0124    35:(TDUP  )   1  62
+0x0002EEA7 | 0128    35:(TDUP  )   2  61
+0x0002EEAB | 012C    3D:(TSETS )   2   1  63  ; "*^*"
+0x0002EEAF | 0130    35:(TDUP  )   2  64
+0x0002EEB3 | 0134    3E:(TSETB )   2   1   1
+0x0002EEB7 | 0138    3D:(TSETS )   1   0  65  ; "^j^"
+0x0002EEBB | 013C    35:(TDUP  )   1  67
+0x0002EEBF | 0140    35:(TDUP  )   2  66
+0x0002EEC3 | 0144    3D:(TSETS )   2   1  68  ; "*_*"
+0x0002EEC7 | 0148    35:(TDUP  )   2  69
+0x0002EECB | 014C    3E:(TSETB )   2   1   1
+0x0002EECF | 0150    3D:(TSETS )   1   0  70  ; "^k^"
+0x0002EED3 | 0154    37:(GSET  )   0  71      ; "FF"
+0x0002EED7 | 0158    4B:(RET0  )   0   1
+
+-- Proto #5 -- pos: 0x2F036, size: 0x1FC, fullsize: 0x1FE, flags: 0x3, params: 0, framesize: 0xB, numuv: 0, numkgc: 22, numkn: 1, numbc: 59, ins: 0x2F03F
+0x0002F03F | 0000    29:(KSHORT)   0   2
+0x0002F043 | 0004    09:(ISNEN )   0   0      ; 1
+0x0002F047 | 0008    58:(JMP   )   0 => 0084
+0x0002F04B | 000C    36:(GGET  )   0   0      ; "48Ž
+MkÉ‰9Ý8ëg“³"
+0x0002F04F | 0010    2C:(KNIL  )   1  10
+0x0002F053 | 0014    42:(CALL  )   0   1  11
+0x0002F057 | 0018    36:(GGET  )   0   1      ; "TÆê5ë>jø@q—“ÏýÇæ9„<%<¥PÀ»KþA"
+0x0002F05B | 001C    2C:(KNIL  )   1   9
+0x0002F05F | 0020    42:(CALL  )   0   1  10
+0x0002F063 | 0024    36:(GGET  )   0   2      ; "Î«•—íckWƒàY9œÞh!vV¿"
+0x0002F067 | 0028    DB:(UNKNW ) 215  37  83
+0x0002F06B | 002C    39:(TGETS )  93 230 154
+0x0002F06F | 0030    84:(UNKNW )     
+0x0002F073 | 0034    A4:(UNKNW ) 194 188  56
+0x0002F077 | 0038    FE:(UNKNW ) 217 
+0x0002F07B | 003C    B9:(UNKNW ) 199  63 248  ; UNKWN
+0x0002F07F | 0040    91:(UNKNW )     
+0x0002F083 | 0044    E0:(UNKNW )  35 114 213
+0x0002F087 | 0048    08:(ISEQN )  21 46646
+0x0002F08B | 004C    9C:(UNKNW )     
+0x0002F08F | 0050    0C:(ISTC  ) 156 60359
+0x0002F093 | 0054    FD:(UNKNW )  47 197  77
+0x0002F097 | 0058    45:(ITERC ) 153 139  17
+0x0002F09B | 005C    49:(RETM  )   3 13701
+0x0002F09F | 0060    15:(LEN   ) 150 54990
+0x0002F0A3 | 0064    70:(UNKNW )     
+0x0002F0A7 | 0068    6F:(UNKNW )     
+0x0002F0AB | 006C    BD:(UNKNW )     148  55
+0x0002F0AF | 0070    58:(JMP   ) 243 => FFFFFFFFFFFF3B54
+0x0002F0B3 | 0074    EC:(UNKNW )  15 124  58
+0x0002F0B7 | 0078    36:(GGET  )   0   9      ; "·!>BæÑ¬ÛbÕoG§ß¡%"
+0x0002F0BB | 007C    2C:(KNIL  )   1   9
+0x0002F0BF | 0080    42:(CALL  )   0   1  10
+0x0002F0C3 | 0084 => 36:(GGET  )   0  10      ; "string"
+0x0002F0C7 | 0088    39:(TGETS )   0   0  11  ; "len"
+0x0002F0CB | 008C    36:(GGET  )   1  12      ; "tonumber"
+0x0002F0CF | 0090    36:(GGET  )   2  10      ; "string"
+0x0002F0D3 | 0094    39:(TGETS )   2   2  13  ; "reverse"
+0x0002F0D7 | 0098    36:(GGET  )   3  10      ; "string"
+0x0002F0DB | 009C    39:(TGETS )   3   3  14  ; "char"
+0x0002F0DF | 00A0    34:(TNEW  )   4   3
+0x0002F0E3 | 00A4    33:(FNEW  )   5  15      ; UNKWN
+0x0002F0E7 | 00A8    3E:(TSETB )   5   4   1
+0x0002F0EB | 00AC    37:(GSET  )   4  16      ; "D"
+0x0002F0EF | 00B0    34:(TNEW  )   4   3
+0x0002F0F3 | 00B4    33:(FNEW  )   5  17      ; UNKWN
+0x0002F0F7 | 00B8    3E:(TSETB )   5   4   1
+0x0002F0FB | 00BC    33:(FNEW  )   5  18      ; UNKWN
+0x0002F0FF | 00C0    3E:(TSETB )   5   4   2
+0x0002F103 | 00C4    37:(GSET  )   4  19      ; "G"
+0x0002F107 | 00C8    34:(TNEW  )   4   3
+0x0002F10B | 00CC    33:(FNEW  )   5  20      ; UNKWN
+0x0002F10F | 00D0    3E:(TSETB )   5   4   1
+0x0002F113 | 00D4    37:(GSET  )   4  21      ; "J"
+0x0002F117 | 00D8    36:(GGET  )   4  19      ; "G"
+0x0002F11B | 00DC    3A:(TGETB )   4   4   2
+0x0002F11F | 00E0    42:(CALL  )   4   1   1
+0x0002F123 | 00E4    32:(UCLO  )   0 => 00E8
+0x0002F127 | 00E8 => 4B:(RET0  )   0   1
+
